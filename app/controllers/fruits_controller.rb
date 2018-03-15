1313ebf969db790718c3991060aa795f9ff2b894
Matt Huntington <matt.huntington@gmail.com>
2018-03-15 10:26:34 -0400

    array of fruit objects

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -2,6 +2,10 @@ class FruitsController < ApplicationController
     def index
         # render({ :json => { :message => 'hi', :status => 200 } })
         # render json: message: 'hi', status: 200 # doesn't work because nested objects are unguessable
-        render json: { message: 'hi', status: 200 }
+        render json: [
+            { name: 'apple', color: 'red', readyToEat: true },
+            { name: 'banana', color: 'yellow', readyToEat: false },
+            { name: 'pineapple', color: 'yelow', readyToEat: true }
+        ]
     end
 end

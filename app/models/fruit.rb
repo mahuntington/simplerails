362c4189fec180f4a5edd6d88b25e7fe7c5e68ae
Matt Huntington <matt.huntington@gmail.com>
2018-03-15 21:41:06 -0400

    trying to connect to heroku db

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,6 +1,11 @@
 class Fruit
-    DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
-
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'simplerails')
+    end
+    
     def initialize(opts = {})
         @id = opts["id"].to_i
         @name = opts["name"]
